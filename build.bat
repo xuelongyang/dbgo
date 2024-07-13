4b55e901f776469708b15bb2697c640f82aa9d7d
xuelongyang <994892792@qq.com>
2024-07-13 18:29:26 +0800

    修改readme

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 set BUILD_DIR=.\release
+set PROJECT_DIR=.
 
 rem Check if build directory exists
 if exist %BUILD_DIR% (
@@ -15,16 +16,22 @@ rem Set GOOS to linux and GOARCH to 386
 go env -w GOOS=linux
 go env -w GOARCH=386
 go build -o %BUILD_DIR%\linux-x86_64\dbgo
+copy %PROJECT_DIR%\LICENSE %BUILD_DIR%\linux-x86_64\LICENSE
+copy %PROJECT_DIR%\README.md %BUILD_DIR%\linux-x86_64\README.md
 
 rem Set GOOS to linux and GOARCH to arm64
 go env -w GOOS=linux
 go env -w GOARCH=arm64
 go build -o %BUILD_DIR%\linux-arm64\dbgo
+copy %PROJECT_DIR%\LICENSE %BUILD_DIR%\linux-arm64\LICENSE
+copy %PROJECT_DIR%\README.md %BUILD_DIR%\linux-arm64\README.md
 
 rem Set GOOS to windows and GOARCH to amd64
 go env -w GOOS=windows
 go env -w GOARCH=amd64
 go build -o %BUILD_DIR%\windows-amd64\dbgo.exe
+copy %PROJECT_DIR%\LICENSE %BUILD_DIR%\windows-amd64\LICENSE
+copy %PROJECT_DIR%\README.md %BUILD_DIR%\windows-amd64\README.md
 
 rem Reset GOOS and GOARCH
 go env -u GOOS
